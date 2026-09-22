82e91e6da71f238773825f09fff510d3816d3180
JeremyByrdyCheron <jbc60510@outlook.fr>
2026-09-22 10:17:44 +0200

    Fix : replace name and firstname by pseudo.

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,17 +4,14 @@ import "./Profile.scss";
 
 function Profile() {
   const data = {
-    firstname: "Jeremy",
-    lastname: "BYRDY--CHERON",
+    pseudo: "Jeremy",
     createdMusics: ["tralala", "trululu", "trololo", "trilili"],
   };
 
   return (
     <div className="profile">
       <img src={ProfileLogo} alt="profil" className="profile__profileLogo" />
-      <h1 className="profile__title">
-        {data.firstname} {data.lastname}
-      </h1>
+      <h1 className="profile__title">{data.pseudo}</h1>
       <hr />
       <h2 className="profile__lastMusicsTitle">Derniers morceaux créés</h2>
       <div className="profile__musicsGrid">
