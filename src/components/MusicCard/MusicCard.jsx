4b1ee5fe513f68534634eefcbc7b8fa454a02277
KillianM18 <killian.mahut@sfr.fr>
2026-09-25 10:19:07 +0200

    feat(profile): desactiver lien des cartes, ajout bouton deconnexion fonctionnel

--- src/components/MusicCard/MusicCard.jsx
+++ src/components/MusicCard/MusicCard.jsx
@@ -1,12 +1,16 @@
 import "./MusicCard.scss";
 import { Link } from "react-router-dom";
 
-function MusicCard({ name }) {
+function MusicCard({ name, isClickable = true }) {
   return (
-    <div className="musicCard">
-      <Link to="/">
-        {name}
-      </Link>
+    <div className={`musicCard ${!isClickable ? 'musicCard--non-clickable' : ''}`}>
+      {isClickable ? (
+        <Link to="/">
+          {name}
+        </Link>
+      ) : (
+        <span className="musicCard__name">{name}</span>
+      )}
     </div>
   );
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,7 +3,11 @@ import MusicHoverButton from "../../components/MusicHoverButton/MusicHoverButton
 import ProfileLogo from "../../assets/images/user.svg";
 import "./Profile.scss";
 
+import { useNavigate } from "react-router-dom";
+
 function Profile() {
+  const navigate = useNavigate();
+  
   const data = {
     pseudo: "Jeremy",
     createdMusics: ["tralala", "trululu", "trololo", "trilili"],
@@ -13,10 +17,18 @@ function Profile() {
     }
   };
 
+  const handleLogout = () => {
+    // Plus tard : ajouter la suppression du token d'authentification
+    navigate("/home_connexion");
+  };
+
   return (
     <div className="profile">
-      <div className="profile__subscriptionButton">
+      <div className="profile__header-actions">
         <MusicHoverButton to="/subscription" label="Abonnement" />
+        <button className="profile__logout-btn" onClick={handleLogout}>
+          Déconnexion
+        </button>
       </div>
       <div className="profile__header">
         <img src={ProfileLogo} alt="profil" className="profile__profileLogo" />
@@ -25,7 +37,7 @@ function Profile() {
       <h2 className="profile__lastMusicsTitle">Derniers morceaux créés</h2>
       <div className="profile__musicsGrid">
         {data.createdMusics.map((music) => {
-          return <MusicCard key={music} name={music} />;
+          return <MusicCard key={music} name={music} isClickable={false} />;
         })}
       </div>
     </div>
